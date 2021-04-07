7b0448a793c72d1228d5a60024993e744097b1ca
Jd's Code Lab <jdscodelab@gmail.com>
2021-04-07 14:00:41 +0530

    Add files via upload

---
+++ CustomControl/fileDetail.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileUploaderUI.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for fileDetail.xaml
+    /// </summary>
+    public partial class fileDetail : UserControl
+    {
+        public fileDetail()
+        {
+            InitializeComponent();
+        }
+
+        public string FileName
+        {
+            get { return (string)GetValue(FileNameProperty); }
+            set { SetValue(FileNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FileName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileNameProperty =
+            DependencyProperty.Register("FileName", typeof(string), typeof(fileDetail));
+
+        public string FileSize
+        {
+            get { return (string)GetValue(FileSizeProperty); }
+            set { SetValue(FileSizeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FileSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileSizeProperty =
+            DependencyProperty.Register("FileSize", typeof(string), typeof(fileDetail));
+
+
+
+        public int UploadProgress
+        {
+            get { return (int)GetValue(UploadProgressProperty); }
+            set { SetValue(UploadProgressProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UploadProgress.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UploadProgressProperty =
+            DependencyProperty.Register("UploadProgress", typeof(int), typeof(fileDetail));
+
+
+
+        public int UploadSpeed
+        {
+            get { return (int)GetValue(UploadSpeedProperty); }
+            set { SetValue(UploadSpeedProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UploadSpeed.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UploadSpeedProperty =
+            DependencyProperty.Register("UploadSpeed", typeof(int), typeof(fileDetail));
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using FileUploaderUI.CustomControl;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace FileUploaderUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Multiselect = true };
+            bool? response = openFileDialog.ShowDialog();
+            if (response == true)
+            {
+                //Get Selected Files
+                string[] files = openFileDialog.FileNames;
+
+                //Iterate and add all selected files to upload
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string filename = System.IO.Path.GetFileName(files[i]);
+                    FileInfo fileInfo = new FileInfo(files[i]);
+                    UploadingFilesList.Items.Add(new fileDetail() 
+                    { 
+                        FileName = filename,
+
+                        //To Convert bytes to Mb => bytes / 1.049e+6
+                        FileSize = string.Format("{0} {1}", (fileInfo.Length/1.049e+6).ToString("0.0"), "Mb"),
+                        UploadProgress=100
+                    });
+                }
+            }
+        }
+
+        private void Rectangle_Drop(object sender, DragEventArgs e)
+        {
+            //Checking what kind of file is User dropping 
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                //Iterate and add all selected files to upload
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string filename = System.IO.Path.GetFileName(files[i]);
+                    FileInfo fileInfo = new FileInfo(files[i]);
+                    UploadingFilesList.Items.Add(new fileDetail()
+                    {
+                        FileName = filename,
+
+                        //To Convert bytes to Mb => bytes / 1.049e+6
+                        FileSize = string.Format("{0} {1}", (fileInfo.Length / 1.049e+6).ToString("0.0"), "Mb"),
+                        UploadProgress = 100
+                    });
+                }
+            }
+        }
+    }
+}
